601e1c42deaede4c76d1fe8a7c8906e0b64a35f3
takuma <k023g0015@g.neec.ac.jp>
2024-07-28 15:37:43 +0900

    02_03 完了

--- main.cpp
+++ main.cpp
@@ -2,6 +2,111 @@
 #include "MyMath.h"
 #include "ImGui.h"
 
+struct Plane {
+	Vector3 normal;
+	float distance;
+};
+
+struct Segment {
+	Vector3 origin;
+	Vector3 diff;
+	unsigned int color;
+};
+
+void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix) {
+	const float kGridHalfWidth = 2.0f;
+	const uint32_t kSubdivision = 10;
+	const float kGridEvery = (kGridHalfWidth * 2.0f) / float(kSubdivision);
+
+	for (uint32_t xIndex = 0; xIndex <= kSubdivision; ++xIndex) {
+		float x = -kGridHalfWidth + (xIndex * kGridEvery);
+		unsigned int color = 0xAAAAAAFF;
+		Vector3 start{ x,0.0f,-kGridHalfWidth };
+		Vector3 end{ x,0.0f,kGridHalfWidth };
+
+		Vector3 startScreen = Transform(Transform(start, viewProjectionMatrix), viewportMatrix);
+		Vector3 endScreen = Transform(Transform(end, viewProjectionMatrix), viewportMatrix);
+
+		if (x == 0.0f) {
+			color = BLACK;
+		}
+
+		Novice::DrawLine(int(startScreen.x), int(startScreen.y), int(endScreen.x), int(endScreen.y), color);
+	}
+
+	for (uint32_t zIndex = 0; zIndex <= kSubdivision; ++zIndex) {
+		float z = -kGridHalfWidth + (zIndex * kGridEvery);
+		unsigned int color = 0xAAAAAAFF;
+
+		Vector3 start{ -kGridHalfWidth,0.0f,z };
+		Vector3 end{ kGridHalfWidth,0.0f, z };
+
+		Vector3 startScreen = Transform(Transform(start, viewProjectionMatrix), viewportMatrix);
+		Vector3 endScreen = Transform(Transform(end, viewProjectionMatrix), viewportMatrix);
+
+		if (z == 0.0f) {
+			color = BLACK;
+		}
+
+		Novice::DrawLine(int(startScreen.x), int(startScreen.y), int(endScreen.x), int(endScreen.y), color);
+	}
+
+}
+
+void DrawPlane(const Plane& plane, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color) {
+
+	Vector3 center = Multiply(plane.distance, plane.normal); // 1
+	Vector3 perpendiculars[4];
+	perpendiculars[0] = Normalize(Perpendicular(plane.normal)); // 2
+	perpendiculars[1] = { -perpendiculars[0].x, -perpendiculars[0].y, -perpendiculars[0].z }; // 3
+	perpendiculars[2] = Cross(plane.normal, perpendiculars[0]); // 4
+	perpendiculars[3] = { -perpendiculars[2].x,-perpendiculars[2].y,-perpendiculars[2].z }; // 5
+	// 6
+	Vector3 points[4];
+	for (uint32_t index = 0; index < 4; ++index) {
+		Vector3 extend = Multiply(2.0f, perpendiculars[index]);
+		Vector3 point = Add(center, extend);
+		points[index] = Transform(Transform(point, viewProjectionMatrix), viewportMatrix);
+	}
+	// pointsをそれぞれ結んでDrawLine矩形を描画
+	 // 正方形を描画
+	Novice::DrawLine(int(points[0].x), int(points[0].y), int(points[2].x), int(points[2].y), color);
+	Novice::DrawLine(int(points[2].x), int(points[2].y), int(points[1].x), int(points[1].y), RED);
+	Novice::DrawLine(int(points[1].x), int(points[1].y), int(points[3].x), int(points[3].y), GREEN);
+	Novice::DrawLine(int(points[3].x), int(points[3].y), int(points[0].x), int(points[0].y), BLUE);
+}
+
+bool IsCollision(const Segment& segment, const Plane& plane) {
+	// 線分の方向ベクトルと平面の法線ベクトルの内積を計算
+	float dot = Dot(plane.normal, segment.diff);
+
+	// 内積が0の場合、線分は平面に平行である
+	if (dot == 0.0f) {
+		return false;
+	}
+
+	// 線分が平面と交差する点を計算
+	float t = (plane.distance - Dot(segment.origin, plane.normal)) / dot;
+
+	// 交点が線分の範囲内にあるかどうかを確認
+	if (t >= 0.0f && t <= 1.0f) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+void DrawLine(const Vector3& p1, const Vector3& p2, const Matrix4x4& wvpMatrix, const Matrix4x4& viewportMatrix, int32_t color) {
+
+	Vector3 startPos = Transform(p1, wvpMatrix);
+	Vector3 endPos = Transform(p2, wvpMatrix);
+
+	startPos = Transform(startPos, viewportMatrix);
+	endPos = Transform(endPos, viewportMatrix);
+
+	Novice::DrawLine(int(startPos.x), int(startPos.y), int(endPos.x), int(endPos.y), color);
+}
+
 const char kWindowTitle[] = "LE2C_08_オカムラ_タクマ_タイトル";
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -11,8 +116,22 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
+
+	Vector3 cameraTranslate{ 0.0f,1.9f,-6.49f };
+	Vector3 cameraRotate{ 0.26f,0.0f,0.0f };
+
+	Plane plane{
+		{0.0f,1.0f,0.0f},
+		0.5f
+	};
+
+	Segment segment{
+		{0.0f,1.7f,0.0f},
+		{0.0f,1.0f,0.0f},
+		WHITE
+	};
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -27,6 +146,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		ImGui::Begin("Segment Controls");
+		ImGui::DragFloat3("Origin", &segment.origin.x, 0.1f);
+		ImGui::DragFloat3("Diff", &segment.diff.x, 0.1f);
+		ImGui::End();
+
+		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
+		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, 1280.0f / 720.0f, 0.1f, 100.0f);
+		Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(0.0f, 0.0f, 1280.0f, 720.0f, 0.0f, 1.0f);
+
+		if (IsCollision(segment, plane)) {
+			segment.color = GREEN;
+		} else {
+			segment.color = WHITE;
+		}
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -34,6 +170,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		DrawGrid(viewProjectionMatrix, viewportMatrix);
+		DrawPlane(plane, viewProjectionMatrix, viewportMatrix, WHITE);
+		DrawLine(segment.origin, segment.diff, viewProjectionMatrix, viewportMatrix, segment.color);
 
 		///
 		/// ↑描画処理ここまで
